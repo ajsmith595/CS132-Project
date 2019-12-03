c060b9bfe0f43eeb833e7545ccd02086b4709810
Adam Smith <Adam.Smith.2@warwick.ac.uk>
2019-12-03 17:55:58 +0000

    Sorta working now lol

--- constants.c
+++ constants.c
@@ -1,12 +1,12 @@
 #ifndef CONSTANTS_C
 #define CONSTANTS_C
 
-#define	NUM_OF_CUBES 3.0f
-#define	ARM_HEIGHT 3.0f
-#define ARM_BASE_TO_CUBES_DIST 5.0f
-#define ARM_SERVO_LENGTH 2.5f
-#define BLOCK_HEIGHT 1.0f
+#define	NUM_OF_CUBES 3
+#define	ARM_HEIGHT 9.0f
+#define ARM_BASE_TO_CUBES_DIST 20.0f
+#define ARM_SERVO_LENGTH 10.7f
+#define BLOCK_HEIGHT 2.5f
 #define PI 3.14596f
-#define BLOCK_Y_OFFSET 0.5f
+#define BLOCK_Y_OFFSET 0
 
 #endif
--- newRobot.c
+++ newRobot.c
@@ -24,6 +24,27 @@ struct Point *baseOfArm;
 int* poleHeights;
 
 
+void reset_arm(){
+        unsigned char arr[] = { 0xff, 0xff, 0x00, 0x02, 0x06, 0xf7};
+
+        int buff_len = 100;
+        unsigned char buff[buff_len];
+
+        int bytes_read = write_to_connection(connection,arr,6,buff,buff_len);
+}
+
+void reset(){
+	float y = ARM_HEIGHT;
+	int i;
+	for(i = 0; i < 3; i++){
+		servos[i]->a->x = 0;
+		servos[i]->a->y = y;
+		y += ARM_SERVO_LENGTH;
+		servos[i]->b->x = 0;
+		servos[i]->b->y = y;
+	}
+}
+
 int robotArmServoValueToDegrees(float x){
 	return (int)((x-511)*150/511);
 }
@@ -71,11 +92,11 @@ void drop(){
 }
 
 void setPole(struct Pole thePole){
-	mtl(1, thePole.pos);	
+	mtl(1, thePole.pos);
 }
 
-void setServoAngle(unsigned char id, float deg){
-	mtl(id, radsToRobotArmServoValue(deg));
+void setServoAngle(unsigned char id, float rads){
+	mtl(id, radsToRobotArmServoValue(rads));
 }
 
 
@@ -97,8 +118,22 @@ void setServoHeightsArr(int blockNumber){
 	}
 	free(offset);
 }
+float abso(float f){
+	if(f > 0){
+		return f;
+	}
+	return -f;
+}
 
+float norm(float f){
+	while(f >= 2 * PI){
+		f -= 2 * PI;
+	}
+	return f;
+}
 void setVertical(int block){
+	reset();
+	printf("Setting vertical to: %d\n", block);
 	int i;
 	for(i = 0; i < 3; i++){ // Doing it 3 times ensures it's in the right place
 		setServoHeightsArr(block);
@@ -106,10 +141,16 @@ void setVertical(int block){
 	int prevAngle = 0;
 	for(i = 0; i < 3; i++){
 		struct Point *diff = subtractPoints(servos[i]->b, servos[i]->a);
-		float angle = PI / 2 - getPointAngle(diff);
-		printf("Angle: %.6f\n", radsToDegrees(angle - prevAngle));
-		//setServoAngle(i + 2, angle - prevAngle);
+		//float angle = -PI / 6;
+		float angle = -abso(getPointAngle(diff));
+		// if(block == 3){
+		// 	angle = getPointAngle(diff);
+		// }
+		printf("Angle(%d): %.6f\n", i + 2, radsToDegrees(getPointAngle(diff) - prevAngle));
+
+		setServoAngle(6-(i + 2), angle - prevAngle);
 		prevAngle = angle;
+
 	}
 }
 
@@ -125,7 +166,7 @@ int moveBlock(struct Pole from, struct Pole to)
     setPole(from);
     wait(1);
     setVertical(poleHeights[from.index]--);
-    wait(1);
+    wait(2);
     grab();
     wait(1);
     toSafeHeight();
@@ -183,7 +224,7 @@ void init(){
 		printf("\n");
 	}
 
-	setVertical(1);
+
 	for(i = 0; i < 3; i++){
 		printServo(servos[i]);
 	}
@@ -198,11 +239,15 @@ void init(){
 
 
 int main(int argc, char* argv[]){
-//	connection = open_connection("/dev/ttyUSB0", B1000000);
-	
+	connection = open_connection("/dev/ttyUSB0", B1000000);
+
+	reset_arm();
 	init();
-	
-	solveHanoi(NUM_OF_CUBES, pole1, pole2, pole3);	
+	reset();
+	drop();
+	toSafeHeight();
+	wait(2);
+	solveHanoi(NUM_OF_CUBES, pole1, pole2, pole3);
 
 	int cubeNum = 1;
 //	setVertical(cubeNum);
